d70bb2871c3acd88a41e752d9c75f234e2cbbeb3
minghao.zl <minghao.zl@alibaba-inc.com>
2014-07-14 20:52:27 +0800

    学习笔记

--- wt.m
+++ wt.m
@@ -1,3 +1,10 @@
 #!/bin/sh
-myDate=$(date +%Y%m%d)
-vi "blogs/$myDate.txt"
+mDate=$(date +%Y-%m-%d)
+
+if [ ! -f "blogs/$mDate.txt" ];
+then
+echo -e "ʱ�䣺$mDate">"blogs/$mDate.txt"
+vi "blogs/$mDate.txt"
+else 
+vi "blogs/$mDate.txt"
+fi
